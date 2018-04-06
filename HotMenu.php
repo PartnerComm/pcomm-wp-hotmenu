02cf19500c00156c3c99790d5fcbba6ccdf6f2e3
Phil Palmieri <ppalmieri@partnercomm.net>
2018-04-06 13:19:26 -0400

    Initial Build

---
+++ HotMenu.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: PartnerComm HotMenu
+Description: Add Quick Hot Menus Based On Taxonomy and Term Pairs
+Version: 1.0.0
+Author: Phil Palmieri
+Author Email: ppalmieri@partnercomm.net
+Text Domain: partnercomm-hotmenu
+License:     GPL2
+ 
+PartnerComm HotMenu is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+PartnerComm HotMenu is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+GNU General Public License https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+namespace PComm\WP\Plugins;
+
+class HotMenu {
+
+  public function init()
+  {
+    add_shortcode( 'hotmenu', [$this, 'doShortcode'] );
+  }
+
+  public function doShortcode($atts)
+  {
+    foreach($atts as $tax => $term) {
+      if(term_exists($term, $tax)) {
+          $taxMatches[] = [
+              'taxonomy' => $tax,
+              'field' => 'slug',
+              'terms' => $term
+          ];
+      }
+    }
+
+    $the_query = new \WP_Query( ['tax_query' => $taxMatches] );
+    $baseLink = (!empty($atts['base-link'])) ? $atts['base-link'] : '';
+    $menu = "<nav class='hotmenu'><ul>";
+    while ( $the_query->have_posts() ) : 
+      $the_query->the_post();
+      $slug = get_post_field( 'post_name', get_post() );
+      $url = $baseLink . '#' . $slug;
+      $menu .= "<li><a href='{$url}' target='_self'>".get_the_title()."</a></li>";
+    endwhile;
+    $menu .= "</ul></nav>";
+
+    return $menu;
+  }
+
+}
+
+$hotMenu = new HotMenu();
+add_action( 'init', [$hotMenu, 'init'] );
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# pcomm-wp-hotmenu
+# PartnerComm Hot Menus
 Build Menu Links Based on Matched Taxonomies
+
+# Requirments
+- PHP 5.6
+- Composer
+- WordPress 4.*
+
+# Usage
+1) Add any targets to your pages based on their slug
+```php
+  $slug = get_post_field( 'post_name', get_post() );
+  <a id="<?php echo $slug;?>"></a>
+```
+2) From within your post or body add the following shortcode
+```text
+  [hotmenu taxonomy="term" base-link='/other/page/to/link/to']
+```
+_You can add multiple taxonomy/term combination, and they will be exclusionary_
+```text
+  [hotmenu taxonomy="term" taxonomy2="term2" taxonomy3="term3" base-link='/other/page/to/link/to']
+```
+3) You can choose to exclude the base-link option, and it will default to just `#target` on the current page
